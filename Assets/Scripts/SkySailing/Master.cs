844ec4f75b4fb3d57d52dbea26bb1d658574449b
Tomoki-Japan <tomoki.kurikuri.1122@gmail.com>
2022-09-15 09:23:01 +0900

    SkySailingのSceneの初期構築

---
+++ Assets/Scripts/SkySailing/Master.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using util;
+using game;
+
+namespace SkySailing
+{
+
+    public class Master : MonoBehaviour
+    {   
+        const int MAX_LOG_LINES = 10;
+
+        public Queue<string> viewerTextQueue = new Queue<string>();
+
+        [SerializeField]
+        private GameObject viewerObject;
+
+        private webSocketClient _socketClient;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _socketClient = GameObject.FindWithTag("webSocketClient").GetComponent<webSocketClient>();         
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //ワイヤ巻き取り用ボタンイベント
+            if(OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
+            {
+                reelWire();
+            }
+            if(OVRInput.GetUp(OVRInput.RawButton.LIndexTrigger))
+            {
+                restore();
+            }
+
+            writeLog();
+            
+        }
+
+        //VR空間上のログ情報に追加
+        public void addLog(string message)
+        {
+            
+            viewerTextQueue.Enqueue(message);
+
+            if(viewerTextQueue.Count > MAX_LOG_LINES)
+            {
+                
+                string hoge = viewerTextQueue.Dequeue();
+            }
+        }
+
+        private void writeLog()
+        {
+            string[] arr = viewerTextQueue.ToArray();
+            string writeText = "";
+            for(int i = 0;i < arr.Length; ++i)
+            {
+                writeText += arr[i] + "\n";
+            }
+            viewerObject.GetComponent<Text>().text = writeText;
+        }
+
+        //ワイヤを巻き取る
+        private void reelWire()
+        {
+            SendingDataFormat data = new SendingDataFormat();
+            data.setTorque(0.75f);
+            _socketClient.sendData(data);
+        }
+
+
+        //モーターの現状復帰用関数(速度0指令)
+        private void restore()
+        {
+            SendingDataFormat data = new SendingDataFormat();
+            data.setSpeed(0.0f);
+            _socketClient.sendData(data);
+        }
+        
+    }
+}
