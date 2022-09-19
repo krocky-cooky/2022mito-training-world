6825d7414e675b3de867686b33c066e7e70ff09b
Tomoki-Japan <tomoki.kurikuri.1122@gmail.com>
2022-09-19 19:05:17 +0900

    Revert "コース作成"
    This reverts commit 668df0d38255b3b652a0866339ec671b8dc7403b.

--- Assets/Scripts/skySailing/game/SailingShip.cs
+++ Assets/Scripts/skySailing/game/SailingShip.cs
@@ -14,7 +14,6 @@ namespace skySailing.game
         public Transform nextShipTransform;
         public float maxXRotationOfPillar;
         public float minXRotationOfPillar;
-        // public bool duringRace = false;
 
         private Vector3 moveVector;
 
@@ -31,29 +30,16 @@ namespace skySailing.game
         // x軸周りの角度の相対位置を受け取ると、それに合わせて柱の角度変更
         // 例えばx軸周りの角度の可動範囲が-30~30で、相対位置で0.3を受け取ったら、-30+60*0.3で-12に帆の角度が変更
         public void changePillarRotation(float relativeXRotation){
-            float nextAngle = minXRotationOfPillar + (maxXRotationOfPillar - minXRotationOfPillar) * relativeXRotation;
-            float rotationAngle = nextAngle - pillarTransform.eulerAngles.x;
-            if (minXRotationOfPillar < nextAngle & nextAngle < maxXRotationOfPillar){
-                pillarTransform.Rotate(rotationAngle, 0.0f, 0.0f);
-            }
+            pillarTransform.Rotate(minXRotationOfPillar + (maxXRotationOfPillar - minXRotationOfPillar) * relativeXRotation - pillarTransform.localEulerAngles.x, 0, 0);
             Debug.Log("xRotation is "+(pillarTransform.localEulerAngles.x).ToString());
             Debug.Log("next angular is " + (minXRotationOfPillar + (maxXRotationOfPillar - minXRotationOfPillar) * relativeXRotation - pillarTransform.localEulerAngles.x).ToString());
         }
 
-        // 帆の傾きに合わせて船を動かす
         public void move(Master master){
-            moveVector = Quaternion.Euler(pillarTransform.eulerAngles.x, pillarTransform.eulerAngles.y, pillarTransform.eulerAngles.z) * new Vector3(0.0f, 0.0f, -master.windSpeed);
+            moveVector = Quaternion.Euler(pillarTransform.eulerAngles.x, 0.0f, 0.0f) * new Vector3(0.0f, 0.0f, -master.windSpeed);
             shipTransform.position += moveVector;
             Debug.Log("moveVector is " + moveVector.ToString());
         }
-
-        // void OnTriggerEnter(Collider other)
-        // {
-        //     Debug.Log("すり抜けた！ 船が");
-        //     if (other.gameObject.tag == "start"){
-        //         duringRace = true;
-        //         Debug.Log("race start");
-        //     }
-        // }
-        }
+       
+    }
 }
