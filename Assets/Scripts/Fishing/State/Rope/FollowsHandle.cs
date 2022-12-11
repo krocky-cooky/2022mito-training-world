1b50565e3aba43564a552f0f26911341213974e3
Tomoki-Japan <tomoki.kurikuri.1122@gmail.com>
2022-12-11 13:06:05 +0900

    静止ルアーを配置

--- Assets/Scripts/Fishing/State/Rope/FollowsHandle.cs
+++ Assets/Scripts/Fishing/State/Rope/FollowsHandle.cs
@@ -27,8 +27,9 @@ namespace Fishing.State
 
         public override int StateUpdate()
         {
-            ropeStateController.ropeRelayBelowHandleTransform.position = new Vector3(ropeStateController.centerOfHandle.position.x, -ropeStateController.ropeLength, ropeStateController.centerOfHandle.position.z);
-
+            ropeStateController.ropeRelayBelowHandleTransform.position = new Vector3(ropeStateController.centerOfHandle.position.x,ropeStateController.centerOfHandle.position.y - ropeStateController.ropeLength, ropeStateController.centerOfHandle.position.z);
+            ropeStateController.ropeRelayBelowHandleTransform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+            
             if ((int)ropeStateController.masterStateController.CurrentState == (int)MasterStateController.StateType.DuringFishing_Wait){
                 ropeStateController.fixedPosition = ropeStateController.masterStateController.ropeRelayBelowHandle.transform.position;
                 ropeStateController.fixedRotation = ropeStateController.masterStateController.ropeRelayBelowHandle.transform.rotation;
