84d8498e8494d0b21d0bd99e26fe8eb93e62b511
Tomoki-Japan <tomoki.kurikuri.1122@gmail.com>
2023-02-15 02:59:41 +0900

    キャリブレーション中でも勝敗を決するように修正

--- Assets/Scripts/Tsunahiki/ForceGauge/State/Master/Calibration.cs
+++ Assets/Scripts/Tsunahiki/ForceGauge/State/Master/Calibration.cs
@@ -53,9 +53,24 @@ namespace tsunahiki.forceGauge.state
                 return (int)MasterStateController.StateType.Fight;
             }      
 
+            if ((int)masterForForceGauge.opponentData.stateId == (int)TsunahikiStateType.EndOfFight)
+            {   
+                updateResult();
+                return (int)MasterStateController.StateType.EndOfFight;
+            }      
+
             return (int)StateType;
+        }
 
-
+        // 勝敗結果を更新
+        private void updateResult(){
+            if ((int)masterForForceGauge.opponentData.latestWinner == masterForForceGauge.myDeviceId){   
+                masterForForceGauge.victoryCounts += 1;
+            }else if((int)masterForForceGauge.opponentData.latestWinner == (int)TrainingDeviceType.Nothing){
+                masterForForceGauge.drawCounts += 1;
+            }else{
+                masterForForceGauge.defeatCounts += 1;
+            }
         }
     }
 
